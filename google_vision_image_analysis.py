08a8bb65fb3479fabd083089bf1f5b9b628d38e7
ZhengyangTang <43021026+ZhengyangTang@users.noreply.github.com>
2018-09-17 22:22:22 -0400

    Update google_vision_image_analysis.py

--- google_vision_image_analysis.py
+++ google_vision_image_analysis.py
@@ -3,12 +3,12 @@ from google.cloud import vision_v1
 
 client = vision_v1.ImageAnnotatorClient()
 for count in range(10):
-	with open('C:/EC601/EC601_IMAGES/0%d.jpg'%count, 'rb') as image_file:
+	with open('C:/0%d.jpg'%count, 'rb') as image_file:
 		content = image_file.read()
 	image = vision_v1.types.Image(content = content)
 	response = client.web_detection(image = image)
 	detection = response.web_detection
-	file = open('C:/EC601/EC601_GOOGLEVISION/image_analysis.txt','a+',encoding = 'utf-8')
+	file = open('C:/image_analysis.txt','a+',encoding = 'utf-8')
 	file.write('labels for image%d:'%count+'\n')
 	for i in detection.web_entities:
 		file.write(i.description+'\n')
