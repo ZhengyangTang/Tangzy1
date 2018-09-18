e8eb4ea14a104622ae3e616f22086b7d7b10696f
ZhengyangTang <43021026+ZhengyangTang@users.noreply.github.com>
2018-09-17 22:24:08 -0400

    Update get_all_images.py

--- get_all_images.py
+++ get_all_images.py
@@ -5,10 +5,10 @@ import urllib
 import sys
 
 #input keys and secret keys  
-consumer_key = 'eIdSVPYAdVOSy3aCluX9P9P97'  
-consumer_secret = 'EZpKmpK6RpyOSoLEHAubZBh3hud0bZucjIS8WFGR1ISEjT7vhy'  
-access_key = '1038545947972780032-viHFmLkInLs79xOxatMU19qcG4SfTh'  
-access_secret = '2L6m6hxXwkTgFOYg15SJ1nBe9yxMoPMUY1lHgnIt4JpBO'  
+consumer_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'  
+consumer_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'  
+access_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'  
+access_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'  
   
 # upload keys and secret keys
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)  
@@ -22,7 +22,7 @@ def get_tweets(screen_name):
 	public_tweets = api.user_timeline(screen_name = screen_name,count = 200)  
 	for tweet in public_tweets:
 		if 'media' in tweet.entities.keys(): 	  
-			urllib.request.urlretrieve(tweet.entities['media'][0]['media_url'],'C:/EC601/EC601_IMAGES/0%d.jpg'%count)
+			urllib.request.urlretrieve(tweet.entities['media'][0]['media_url'],'C:/0%d.jpg'%count)
 			count = count + 1
 			if count == 10:
 				break
