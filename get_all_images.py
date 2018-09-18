e4dbb97ce41b994dc183a9e65ca282154578e5e1
ZhengyangTang <43021026+ZhengyangTang@users.noreply.github.com>
2018-09-17 22:08:53 -0400

    Add files via upload

--- get_all_images.py
+++ get_all_images.py
@@ -2,12 +2,13 @@
 import tweepy  
 import requests
 import urllib
+import sys
 
 #input keys and secret keys  
-consumer_key = 'xxxxxxxxxxxxxxxxxxxxxx'  
-consumer_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'  
-access_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'  
-access_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'  
+consumer_key = 'eIdSVPYAdVOSy3aCluX9P9P97'  
+consumer_secret = 'EZpKmpK6RpyOSoLEHAubZBh3hud0bZucjIS8WFGR1ISEjT7vhy'  
+access_key = '1038545947972780032-viHFmLkInLs79xOxatMU19qcG4SfTh'  
+access_secret = '2L6m6hxXwkTgFOYg15SJ1nBe9yxMoPMUY1lHgnIt4JpBO'  
   
 # upload keys and secret keys
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)  
@@ -20,14 +21,11 @@ def get_tweets(screen_name):
 	count = 0;
 	public_tweets = api.user_timeline(screen_name = screen_name,count = 200)  
 	for tweet in public_tweets:
-		if 'media' in tweet.entities.keys():	  
-			#image = requests.get(tweet.entities['media'][0]['media_url'])
-			#with open("0%d.jpg"%count,'w') as f:
-			#	f.write(image.content)
-			urllib.request.urlretrieve(tweet.entities['media'][0]['media_url'],'C:/EC601/EC601_IMAGES/%d.png'%count)
+		if 'media' in tweet.entities.keys(): 	  
+			urllib.request.urlretrieve(tweet.entities['media'][0]['media_url'],'C:/EC601/EC601_IMAGES/0%d.jpg'%count)
 			count = count + 1
 			if count == 10:
 				break
 if __name__ == '__main__':
-	get_tweets('@RickandMorty')
+	get_tweets(sys.argv[1])
   
